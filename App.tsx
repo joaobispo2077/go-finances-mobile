50c2a980dc314f78f194ad8432906e0d367eae12
João Bispo <joaobispo2077@gmail.com>
2021-08-30 11:21:30 -0300

    feat: setup typescript

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Welcome } from './src/components/Welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Welcome />
       <StatusBar style="auto" />
     </View>
   );
