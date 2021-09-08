816c3a2e5c3b2abf5e9fc56bdea15cd00c7729a3
João Bispo <joaobispo2077@gmail.com>
2021-09-08 14:59:07 -0300

    feat: add styles into tab navigation bottom

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Platform } from 'react-native';
 
+import { MaterialIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'styled-components/native';
 
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
@@ -8,10 +11,50 @@ import { Register } from '../screens/Register';
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
+  const theme = useTheme();
   return (
-    <Navigator>
-      <Screen name="Listagem" component={Dashboard} />
-      <Screen name="Cadastrar" component={Register} />
+    <Navigator
+      screenOptions={{
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          height: 88,
+        },
+      }}
+    >
+      <Screen
+        name="Listagem"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="Cadastrar"
+        component={Register}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="attach-money" color={color} size={size} />
+          ),
+        }}
+      />
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="pie-chart" color={color} size={size} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
