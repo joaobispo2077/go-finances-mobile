c5618ca8b4d8c56adbd7cc0d83e2d97095795547
João Bispo <joaobispo2077@gmail.com>
2021-09-08 14:39:01 -0300

    feat: setup react navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,4 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
-import { ThemeProvider } from 'styled-components';
-
-import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
+import * as React from 'react';
 
 import {
   useFonts,
@@ -11,6 +6,12 @@ import {
   Poppins_500Medium,
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
+import { NavigationContainer } from '@react-navigation/native';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './src/global/styles/theme';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +26,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
