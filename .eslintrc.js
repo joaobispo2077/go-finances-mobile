768c127cb516dc9a0a114e0d2f495a440cf9c010
João Bispo <joaobispo2077@gmail.com>
2021-09-08 14:39:46 -0300

    chore: add import helpers rule to move react above all imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,7 @@ module.exports = {
       'warn',
       {
         newlinesBetween: 'always',
-        groups: ['module', ['parent', 'sibling', 'index']],
+        groups: ['/^react/', 'module', ['parent', 'sibling', 'index']],
         alphabetize: { order: 'asc', ignoreCase: true },
       },
     ],
