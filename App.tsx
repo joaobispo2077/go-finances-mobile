e73791290ea43c853aebc964af0f5aa58b6bfe49
João Bispo <joaobispo2077@gmail.com>
2021-09-28 16:02:09 -0300

    feat: setup svg into app

--- App.tsx
+++ App.tsx
@@ -15,7 +15,8 @@ import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
 import theme from './src/global/styles/theme';
-import { AppRoutes } from './src/routes/app.routes';
+// import { AppRoutes } from './src/routes/app.routes';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -32,7 +33,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
