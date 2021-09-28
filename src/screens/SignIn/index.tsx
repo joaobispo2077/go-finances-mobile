8df84cc7b8c6af560fcb45c4288f8ad4e6fbf682
João Bispo <joaobispo2077@gmail.com>
2021-09-28 16:22:46 -0300

    feat: add header structure and styles from sign in screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
-import { Container } from './styles';
+import AppleLogo from '../../assets/apple_logo.svg';
+import GoFinancesLogo from '../../assets/gofinances_logotipo.svg';
+import GoogleLogo from '../../assets/google_logo.svg';
+import {
+  Container,
+  Header,
+  Title,
+  TitleWrapper,
+  SignTitle,
+  Footer,
+} from './styles';
 
 export const SignIn = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Header>
+        <TitleWrapper>
+          <GoFinancesLogo width={120} height={68} />
+          <Title>
+            Controle suas{'\n'} finanças de forma{'\n'} muito simples
+          </Title>
+        </TitleWrapper>
+
+        <SignTitle>Faça seu login com{'\n'} uma das contas abaixo</SignTitle>
+      </Header>
+      <Footer>
+        <GoogleLogo width={200} height={200} />
+        <AppleLogo width={200} height={200} />
+      </Footer>
+    </Container>
+  );
 };
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,3 +1,44 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 70%;
+
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const TitleWrapper = styled.View`
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(30)}px;
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.medium};
+
+  text-align: center;
+
+  margin-top: ${RFValue(4)}px;
+`;
+
+export const SignTitle = styled.Text`
+  font-size: ${RFValue(16)}px;
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.regular};
+
+  text-align: center;
+
+  margin-top: ${RFValue(80)}px;
+  margin-bottom: ${RFValue(67)}px;
+`;
+
+export const Footer = styled.View``;
